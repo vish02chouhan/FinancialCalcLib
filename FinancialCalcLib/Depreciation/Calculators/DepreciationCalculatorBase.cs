d29274b610571b8b490c827e944be3710b63110d
Vishwajeet Singh <vishwajeet.chouhan@qpharmacorp.com>
2023-04-06 19:25:27 -0400

    Added back delegate

--- FinancialCalcLib/Depreciation/Calculators/DepreciationCalculatorBase.cs
+++ FinancialCalcLib/Depreciation/Calculators/DepreciationCalculatorBase.cs
@@ -1,14 +1,28 @@
-﻿namespace FinancialCalcLib.Depreciation.Calculators
+﻿using FinancialCalcLib.Depreciation.Delegates;
+using FinancialCalcLib.Depreciation.EventArgs;
+
+namespace FinancialCalcLib.Depreciation.Calculators
 {
     public abstract class DepreciationCalculatorBase : IDepreciationCalculator
     {
-
+        public event DepreciationCalculatedEventHandler DepreciationCalculated = delegate { };
         /// <summary>
         /// Calculate the annual depreciation for a given year. This method must be implemented by the derived class. 
         /// </summary>
         /// <param name="year"></param>
         /// <returns></returns>
         public abstract double CalculateAnnualDepreciation(int year);
+
+        /// <summary>
+        /// Invoke an event to notify the subscribers that the annual depreciation has been calculated.
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="depreciation"></param>
+        protected virtual void OnDepreciationCalculated(int year, double depreciation)
+        {
+            DepreciationCalculated?.Invoke(this, new DepreciationCalculatedEventArgs(year, depreciation));
+        }
+
 
         /// <summary>
         /// Calculate the annual depreciation for a given year. This method must be implemented by the derived class. 
